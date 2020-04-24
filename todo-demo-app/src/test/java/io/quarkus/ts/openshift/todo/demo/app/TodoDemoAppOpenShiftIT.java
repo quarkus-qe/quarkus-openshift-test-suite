38797f5dd31f0be8359c8e3e7a9b13480077d0bf
Ladislav Thon <lthon@redhat.com>
2020-04-24 17:45:44 +0200

    initial version of manual application deployments
    This commit also includes a simple test for the TODO demo app
    with Quarkus, using S2I source build.

---
+++ todo-demo-app/src/test/java/io/quarkus/ts/openshift/todo/demo/app/TodoDemoAppOpenShiftIT.java
@@ -0,0 +1,28 @@
+package io.quarkus.ts.openshift.todo.demo.app;
+
+import io.quarkus.ts.openshift.common.AdditionalResources;
+import io.quarkus.ts.openshift.common.ManualDeployment;
+import io.quarkus.ts.openshift.common.OpenShiftTest;
+import io.quarkus.ts.openshift.common.injection.TestResource;
+import org.junit.jupiter.api.Test;
+
+import java.net.URL;
+
+import static io.restassured.RestAssured.when;
+
+@OpenShiftTest
+@ManualDeployment(appName = "todo-demo-app", httpRoot = "/", knownEndpoint = "/")
+@AdditionalResources("classpath:openjdk-11-rhel7.yaml")
+@AdditionalResources("classpath:todo-demo-app.yaml")
+public class TodoDemoAppOpenShiftIT {
+    @TestResource
+    private URL url;
+
+    @Test
+    public void verify() {
+        when()
+                .get(url)
+        .then()
+                .statusCode(200);
+    }
+}
