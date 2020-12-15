ab84baa95122726c95b161c220989d7f1f72a5e5
pablo gonzalez granados <pagonzal@redhat.com>
2020-12-15 16:14:20 +0100

    Verify Http/2 support

---
+++ http/src/main/java/io/quarkus/ts/openshift/http/GrpcService.java
@@ -0,0 +1,20 @@
+package io.quarkus.ts.openshift.http;
+
+import io.grpc.stub.StreamObserver;
+import io.quarkus.example.GreeterGrpc;
+import io.quarkus.example.HelloReply;
+import io.quarkus.example.HelloRequest;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class GrpcService extends GreeterGrpc.GreeterImplBase{
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        String name = request.getName();
+        String message = "Hello " + name;
+        responseObserver.onNext(HelloReply.newBuilder().setMessage(message).build());
+        responseObserver.onCompleted();
+    }
+}
