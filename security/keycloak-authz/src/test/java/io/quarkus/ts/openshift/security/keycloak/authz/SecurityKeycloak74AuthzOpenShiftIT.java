cefe84ac7e2da5a3d2dbf7067423d8628be71747
Jose <josecarvajalhilario@gmail.com>
2021-01-18 09:54:55 +0100

    Disable Keycloak-Authz test for 7.4

--- security/keycloak-authz/src/test/java/io/quarkus/ts/openshift/security/keycloak/authz/SecurityKeycloak74AuthzOpenShiftIT.java
+++ security/keycloak-authz/src/test/java/io/quarkus/ts/openshift/security/keycloak/authz/SecurityKeycloak74AuthzOpenShiftIT.java
@@ -3,7 +3,9 @@ package io.quarkus.ts.openshift.security.keycloak.authz;
 import io.quarkus.ts.openshift.common.AdditionalResources;
 import io.quarkus.ts.openshift.common.OnlyIfConfigured;
 import io.quarkus.ts.openshift.common.OpenShiftTest;
+import org.junit.jupiter.api.Disabled;
 
+@Disabled("Caused by https://github.com/quarkusio/quarkus/issues/14318")
 @OpenShiftTest
 @AdditionalResources("classpath:deployments/keycloak/version-74.yaml")
 @AdditionalResources("classpath:keycloak-realm.yaml")
