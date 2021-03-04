0a547d49243e437da2093ef04b71452fe00aa5b5
Michal Jurč <mjurc@redhat.com>
2021-03-04 01:50:45 +0100

    Adding a test application verifying that configuration of multiple persistence units within application.properties works.

---
+++ sql-db/multiple-pus/src/test/java/io/quarkus/ts/openshift/sqldb/multiplepus/MultiplePersistenceUnitTest.java
@@ -0,0 +1,7 @@
+package io.quarkus.ts.openshift.sqldb.multiplepus;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class MultiplePersistenceUnitTest extends AbstractMultiplePersistenceUnitTest {
+}
