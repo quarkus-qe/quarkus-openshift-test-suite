09c927572815996136f260c0fecfc67f0efe4878
Michal Jurč <mjurc@redhat.com>
2021-03-18 10:33:38 +0100

    Skipping MultiplePersistenceUnitTest due to https://github.com/quarkusio/quarkus/issues/15836.

--- sql-db/multiple-pus/src/test/java/io/quarkus/ts/openshift/sqldb/multiplepus/MultiplePersistenceUnitTest.java
+++ sql-db/multiple-pus/src/test/java/io/quarkus/ts/openshift/sqldb/multiplepus/MultiplePersistenceUnitTest.java
@@ -1,7 +1,9 @@
 package io.quarkus.ts.openshift.sqldb.multiplepus;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Disabled;
 
 @QuarkusTest
+@Disabled("https://github.com/quarkusio/quarkus/issues/15836")
 public class MultiplePersistenceUnitTest extends AbstractMultiplePersistenceUnitTest {
 }
