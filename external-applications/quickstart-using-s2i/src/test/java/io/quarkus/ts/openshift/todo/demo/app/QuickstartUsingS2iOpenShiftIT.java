b1ff8368c7e9d212e01cf3e2e4ba108c1cbb4771
Jose <josecarvajalhilario@gmail.com>
2021-05-04 07:32:17 +0200

    Add module to cover the Quickstart using S2i
    This quickstart is part of OpenShift quickstarts.
    More information in QUARKUS-975
    Quarkus Quickstart set branch 1.11

---
+++ external-applications/quickstart-using-s2i/src/test/java/io/quarkus/ts/openshift/todo/demo/app/QuickstartUsingS2iOpenShiftIT.java
@@ -0,0 +1,31 @@
+package io.quarkus.ts.openshift.todo.demo.app;
+
+import io.quarkus.ts.openshift.common.AdditionalResources;
+import io.quarkus.ts.openshift.common.CustomAppMetadata;
+import io.quarkus.ts.openshift.common.OpenShiftTest;
+import io.quarkus.ts.openshift.common.deploy.ManualDeploymentStrategy;
+import io.quarkus.ts.openshift.common.injection.TestResource;
+
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+
+import java.net.URL;
+
+import static io.restassured.RestAssured.when;
+
+@OpenShiftTest(strategy = ManualDeploymentStrategy.class)
+@CustomAppMetadata(appName = "quickstart-using-s2i", httpRoot = "/", knownEndpoint = "/")
+@AdditionalResources("classpath:deployments/maven/s2i-maven-settings.yaml")
+@AdditionalResources("classpath:quickstart-using-s2i.yaml")
+public class QuickstartUsingS2iOpenShiftIT {
+    @TestResource
+    private URL url;
+
+    @Test
+    public void verify() {
+        when()
+                .get(url)
+        .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+}
